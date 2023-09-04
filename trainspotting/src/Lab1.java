87c0af0bd3871835c27a726635b3f7eed077b814
Alva <020319alan@gmail.com>
2023-09-04 16:47:22 +0200

    commit nr2

--- trainspotting/src/Lab1.java
+++ trainspotting/src/Lab1.java
@@ -7,6 +7,15 @@ public class Lab1 {
 
     try {
       tsi.setSpeed(1,speed1);
+      tsi.setSpeed(2,speed2);
+      tsi.setSwitch(17,7, 0);
+      
+      SensorEvent sensor11 = new SensorEvent(1, 17, 7, 1);
+      
+      System.out.println(sensor11);
+
+      
+
     }
     catch (CommandException e) {
       e.printStackTrace();    // or only e.getMessage() for the error
